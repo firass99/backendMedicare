62bc2dfe28906d2ba7f463ee5fbf300e491015a6
iheb <iheb@gmail.com>
2024-05-07 13:05:12 +0100

    iheb first

---
+++ client/src/App.js
@@ -0,0 +1,77 @@
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
+import Spinner from "./components/Spinner";
+import Doctors from "./pages/admin/Doctors";
+import Users from "./pages/admin/Users";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import NotificationPage from "./pages/Notification";
+import Register from "./pages/Register";
+function App() {
+  const { loading } = useSelector((state) => state.alerts);
+  return (
+    <>
+      <BrowserRouter>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute>
+                  <Users />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/doctors"
+              element={
+                <ProtectedRoute>
+                  <Doctors />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/notification"
+              element={
+                <ProtectedRoute>
+                  <NotificationPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+          </Routes>
+        )}
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(moragan("dev"));
+app.use("/admin", require("./routes/adminRoutes"));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
