cbdf797067996df3303fa80479d042404ff013b6
spearroo <firassebai.fs@gmail.com>
2024-05-09 16:30:29 +0100

    finish spec methds and add  admin middleware

---
+++ middlewares/isAdmin.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+//next khatro middlware maandouch retour
+const isAdmin = (req,res,next) =>{
+//header c'est la partie identification de la requete
+    const token = req.header('Authorization');
+
+    try{
+        //extraction du token la partie 1(khater 3ana bearer token donc bch nekhdhou juste token heka 3leh 3mlna split)
+        const tokenData = token.split(' ')[1];
+        const decodedToken = jwt.verify(tokenData, process.env.secret);//secret key
+        if(decodedToken.role=="admin"){
+            next();
+            req.userId=decodedToken._id;
+        }
+    }catch(error){
+        return res.status(401).send('Non Authorized');
+    }
+}
---
+++ routes/specialite.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router= express.Router();
+
+const Specialite = require('../models/specialite');
+
+// name, about
+router.post('/add',async(req,res)=>{
+    try {
+        newData =req.body;
+        newSpec= new Specialite(newData)
+        resp= await newSpec.save()
+
+        res.status(200).send({success:true, data:resp})
+    } catch (err) {
+        res.status(400).send({success:false,error:err})
+        }
+})
+
+
+router.put('/update/:id',async(req,res)=>{
+    try {
+        id=req.params.id
+        Data =req.body;
+        Spec=await Specialite.findByIdAndUpdate({_id:id},Data)
+
+        res.status(200).send({success:true, data:Spec})
+    } catch (err) {
+        res.status(400).send({success:false,error:err})
+        }
+})
+
+
+router.get('/all',async(req,res)=>{
+    try {
+        spec= await Specialite.find()
+        res.status(200).send({success:true, data:spec})
+    } catch (err) {
+        res.status(400).send({success:false, error:err})
+    }
+})
+
+
+router.get('/getById/:id',async(req,res)=>{
+    try {
+        id=req.params.id 
+        spec= await Specialite.findById({'_id':id})
+        res.status(200).send({success:true, data:spec})
+    } catch (err) {
+        res.status(400).send({success:false, error:err})
+    }
+})
+
+
+router.delete('/delete/:id', async(req,res)=>{
+    try {
+        id=req.params.id
+        spec= await Specialite.findByIdAndDelete({'_id':id})
+        res.status(200).send({success:true, data: "Specialite Deleted"})        
+    } catch (err) {
+        res.status(400).send({success:false,error:err})
+    }
+})
+
+
+module.exports=router
