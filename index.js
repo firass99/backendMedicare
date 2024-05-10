d25f3b966864b71d84fbb80d765e3e8e219654c1
spearroo <firassebai.fs@gmail.com>
2024-05-10 01:46:49 +0100

    NOTIF FINISHED X add console paths

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 require('./config/connect');
+const expressListEndpoints = require('express-list-endpoints');
 const userApi=require('./routes/user')
 const consultationApi=require('./routes/consultation')
 const specialiteApi=require('./routes/specialite')
@@ -9,6 +10,8 @@ const notificationApi=require('./routes/notification')
 
 const app=express()
 app.use(express.json())
+
+
 
 //this route MUST HAVE TO GET IMAGES
 app.use('/getImage',express.static('./uploads'))
@@ -19,7 +22,13 @@ app.use('/consultation',consultationApi)
 app.use('/specialite',specialiteApi)
 app.use('/notification',notificationApi)
 
+//this will show all paths
+const endpoints = expressListEndpoints(app);
+endpoints.forEach(item => {
+    console.log('Method :   '+ item.methods + '   ||| '+ ' URL : ' +item.path);
+});
 
 app.listen(3000,()=>{
     console.log('server works at 3000')
 })
+
